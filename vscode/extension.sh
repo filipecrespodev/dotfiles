c5460e4d9f5f071f97a93b40bac2f39f48323b13
Filipe Crespo <filipe.crespo@locaweb.com.br>
2020-04-07 14:42:28 -0300

    add extensions in vscode

--- vscode/extension.sh
+++ vscode/extension.sh
@@ -23,3 +23,4 @@ code --install-extension CoenraadS.bracket-pair-colorizer
 code --install-extension christian-kohler.path-intellisense
 code --install-extension ybaumes.highlight-trailing-white-spaces
 code --install-extension HookyQR.beautify
+code --install-extension sporto.rails-go-to-spec
