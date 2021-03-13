7f8f2bdc5830bd5ad6509f6ef303b6303f3eb22e
Filipe Crespo <filipe.crespo@locaweb.com.br>
2021-03-13 19:53:49 -0300

    update docker

--- installers/docker.sh
+++ installers/docker.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-apt-key fingerprint 0EBFCD88
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+
+apt update
+
+apt install docker-ce docker-ce-cli containerd.io
 
 usermod -aG docker $USER
 
